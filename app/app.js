431830b03430af4b6fbaa1edc8cba00f329212be
VictorJacquinotFega <victor.jacquinot-fega@epitech.eu>
2025-11-02 21:45:54 +0100

    feat(app): trying new things with the app

--- app/app.js
+++ app/app.js
@@ -3,8 +3,7 @@ var app = express();
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello World!');
-  res.send('This is a simple Express server.');
+  res.send("Salut c'est moi Victor");
 });
 
 // Listen
